4361a9f91ea5a5f42c5f8e828c879d1496cc403e
Nikita Kumari <nikita.kumari@avalara.com>
2024-09-17 20:27:22 +0530

    feat: added multiple functions to deal with different types of cases

---
+++ README.md
@@ -0,0 +1,17 @@
+# @nikita-naredi/string-utility
+
+> A utility package to parse strings
+
+## Install
+
+Using npm:
+
+```sh
+npm install --save @nikita-naredi/string-utility
+```
+
+or using yarn:
+
+```sh
+yarn add @nikita-naredi/string-utility
+```
--- index.js
+++ index.js
@@ -1,3 +1,143 @@
-export const test = () => {
-  console.log("test");
-};
+// Handle different casing
+export function toUpperSnakeCase(str) {
+  return str.toUpperCase().replace(/\s+/g, "_");
+}
+
+// const text = "Hello World";
+// const upperSnakeCaseText = toUpperSnakeCase(text);
+// console.log(upperSnakeCaseText);  // Output: "HELLO_WORLD"
+
+export function toReverseCase(str) {
+  return str
+    .split("")
+    .map((char) =>
+      char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()
+    )
+    .join("");
+}
+
+// const text = "Hello World";
+// const reverseCaseText = toReverseCase(text);
+// console.log(reverseCaseText);  // Output: "hELLO wORLD"
+
+export function toAlternatingCase(str) {
+  return str
+    .split("")
+    .map((char, index) =>
+      index % 2 === 0 ? char.toUpperCase() : char.toLowerCase()
+    )
+    .join("");
+}
+
+// const text = "hello world";
+// const alternatingCaseText = toAlternatingCase(text);
+// console.log(alternatingCaseText);  // Output: "HeLlO wOrLd"
+
+export function toCamelCase(str) {
+  const [firstWord, ...otherWords] = str.split(" ");
+  return [
+    firstWord.toLowerCase(),
+    ...otherWords.map(
+      (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+    ),
+  ].join("");
+}
+
+// const text = "hello world";
+// const camelCaseText = toCamelCase(text);
+// console.log(camelCaseText);  // Output: "helloWorld"
+
+export function toPascalCase(str) {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join("");
+}
+
+// const text = "hello world";
+// const pascalCaseText = toPascalCase(text);
+// console.log(pascalCaseText);  // Output: "HelloWorld"
+
+export function toKebabCase(str) {
+  return str.toLowerCase().replace(/\s+/g, "-");
+}
+
+// const text = "Hello World";
+// const kebabCaseText = toKebabCase(text);
+// console.log(kebabCaseText);  // Output: "hello-world"
+
+export function toSnakeCase(str) {
+  return str.toLowerCase().replace(/\s+/g, "_");
+}
+
+// const text = "Hello World";
+// const snakeCaseText = toSnakeCase(text);
+// console.log(snakeCaseText);  // Output: "hello_world"
+
+export function toCapitalizedCase(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) =>
+      word.length > 0 ? word[0].toUpperCase() + word.slice(1) : ""
+    )
+    .join(" ");
+}
+
+// const text = "hello world";
+// const capitalizedCaseText = toCapitalizedCase(text);
+// console.log(capitalizedCaseText);  // Output: "Hello World"
+
+export function toSentenceCase(str) {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+// const sentenceCaseText = toSentenceCase(text);
+// console.log(sentenceCaseText); // Output: "Hello world"
+
+export function toTitleCase(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+// const text = "hello world";
+// const titleCaseText = toTitleCase(text);
+// console.log(titleCaseText);  // Output: "Hello World"
+
+// Remove non alpha character from String
+export function removeNonAlpha(str) {
+  return str.replace(/[^a-zA-Z]/g, "");
+}
+
+export function containsOnlyAlpha(str) {
+  return /^[a-zA-Z]+$/.test(str);
+}
+
+// Mine:
+// findSubstringOccurrences
+// pascalCase
+// camelCase
+// Title Case
+// Sentence Case
+// Capitalized Case
+// Snake Case
+// Kebab Case
+// Pascal Case
+// Camel Case
+// Alternating Case
+// Reverse Case
+// Upper Snake Case
+
+// generate random string of specified length
+// replaceAllOccurrences
+// findLastOccurrence
+// findfirstOccurrence
+// replaceSpecificOccurence
+// charAtFromEnd
+// Extract Substring Between Two Characters
+// is Palindrome
+// Get Substring Before or After Character
